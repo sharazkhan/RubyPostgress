e400f9b48df5d8d9e5880be63136b828120d223c
sharaz <sharazkhanz@gmail.com>
2016-09-04 16:06:06 +0530

    added comments module

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,63 +1,57 @@
 class ArticlesController < ApplicationController
+  # Default Function
+  def index
+    @articles = Article.all
+  end
 
-    # Default Function
-	def index
-		@articles = Article.all
-	end
-	 
-	# New Article - Form
-	def new
-		@article = Article.new
-	end
-	
-	# Edit an Article - Form
-	def edit
-		@article = Article.find(params[:id])
-	end
-	
-	
-	# Save article to datatbase
-	def create
-	  @article = Article.new(article_params)
-	  if @article.save
-		redirect_to @article
-	  else
-		render 'new'
-	  end
-	end
-	 
-	
-	# Show Article
-	def show
-	   @article = Article.find(params[:id])
-	end
-	
-	
-	# Update an Article
-	def update
-	  @article = Article.find(params[:id])
-	 
-	  if @article.update(article_params)
-		redirect_to @article
-	  else
-		render 'edit'
-	  end
-	end
-	
-	
-	# Delete an Article
-	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
-	 
-		redirect_to articles_path
+  # New Article - Form
+  def new
+    @article = Article.new
+  end
+
+  # Edit an Article - Form
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # Save article to datatbase
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
-	 
-	
-	private
-	def article_params
-		params.require(:article).permit(:title, :text)
-	end
-		  
-		 
+  end
+
+  # Show Article
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # Update an Article
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  # Delete an Article
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
- # get 'welcome/index'
-  resources :articles
+# get 'welcome/index'
+# resources :articles
+
+# Routes fo
+# Nested Routes for comments which belongs to article
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
